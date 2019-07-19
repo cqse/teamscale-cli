c055d3be0ac0450c25c0cf07065015daf32ee5fe
Thomas Kinnen <kinnen@cqse.eu>
2019-07-19 09:58:08 +0200

    Prepare for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="teamscale-cli",
-    version="4.9.1",
+    version="5.2.0",
     author="Thomas Kinnen - CQSE GmbH",
     author_email="kinnen@cqse.eu",
     description=("Client for performing precommit analysis with Teamscale."),
