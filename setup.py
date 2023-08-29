7164eb395b415137d3165efba248cae7037e4e29
Jonas Bogenberger <bogenberger@cqse.eu>
2023-08-29 16:47:32 +0200

    Prepare for 9.1.0 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="teamscale-cli",
-    version="8.8.0",
+    version="9.1.0",
     author="Thomas Kinnen - CQSE GmbH",
     author_email="kinnen@cqse.eu",
     description=("Client for performing precommit analysis with Teamscale."),
